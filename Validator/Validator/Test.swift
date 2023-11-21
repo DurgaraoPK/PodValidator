c6ffb2f7537d4322052006348f712db4cf0c71d8
DurgaraoPK <durgarao.pk@gmail.com>
2023-11-21 21:01:57 +0530

    initial commit

---
+++ Validator/Validator/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Validator
+//
+//  Created by DURGA RAO on 21/11/23.
+//
+
+import Foundation
+
+
+public struct Test {
+    
+    public static func testing() {
+        print("Printing test Pods")
+    }
+}
